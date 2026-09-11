a8a4e0b4afe8d336305d055e4615569af4b4bb29
JustRihab <bckrihab@gmail.com>
2026-09-11 12:16:04 +0100

    edit exercice-09.js

--- day02/exercices/exercice-09.js
+++ day02/exercices/exercice-09.js
@@ -20,14 +20,14 @@
 // 2. Écris ta solution sous cette ligne.
 // TODO: écris ta solution ici.
 for ( let i=1; i<=50; i++) {
-    if ( i % 3 == 0 ) {
-        console.log("Fizz");
+    if ( i % 3 == 0 && i % 5 == 0 ) {
+        console.log("FizzBuzz");
     }
     else if ( i % 5 == 0) {
         console.log("Buzz");
     }
-    else if ( i % 3 == 0 && i % 5 == 0 ) {
-        console.log("FizzBuzz");
+    else if ( i % 3 == 0 ) {
+        console.log("Fizz");
     }
     else {
         console.log(i);
